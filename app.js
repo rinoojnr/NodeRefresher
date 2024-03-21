523576a5a19b435c33e8e310e6ed5f065b71b2ab
rinoojnr <rinoojnr@gmail.com>
2024-03-21 17:52:52 +0530

    Core module path

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const os = require('os');
+const path = require('path');
 
 console.log(os.freemem());
 console.log(os.totalmem());
 console.log(os.release());
 console.log(os.type());
 console.log(os.uptime());
+
+
+
+const filePath = path.join('/home', 'anupama', 'Sharpener Main'); // join is used to combine path
+
+console.log(filePath);
+console.log(path.basename(filePath));
